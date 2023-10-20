bfd067a59c6a72bd7e0c6b9fe3888896423de130
khasnahmae <khasnahm@gmail.com>
2023-10-20 15:40:47 +0700

    fix alhamdulillah

--- app/Imports/SuratkeluarImport.php
+++ app/Imports/SuratkeluarImport.php
@@ -19,8 +19,8 @@ class SuratkeluarImport implements WithHeadingRow,ToModel
         return new Suratkeluar([
             'no_surat'=>$row['no_surat'],
             'tgl_surat'=>\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tgl_surat'])->format('Y-m-d'),
-            'tgl_masuk'=>\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tgl_masuk'])->format('Y-m-d'),
-            'pengirim'=>$row['pengirim'],
+            'tgl_keluar'=>\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tgl_keluar'])->format('Y-m-d'),
+            'tujuan'=>$row['tujuan'],
             'ringkasan'=>$row['isi'],
             'file_surat'=>$row['file']
         ]);
